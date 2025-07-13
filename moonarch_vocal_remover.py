6a25afbf3841b7ad6fec45a19472852836f109bb
NisaEngineers <71565114+NisaEngineers@users.noreply.github.com>
2025-07-13 20:32:57 +0600

    Add api_1_13_july folder with model.data-00000-of-00001

---
+++ moonarch_vocal_remover.py
@@ -0,0 +1,27 @@
+from spleeter.separator import Separator
+import os
+
+class VocalRemover:
+    def __init__(self, input_path, task='spleeter:2stems'):
+        self.input_path = input_path
+        self.task = task
+        self.separator = Separator(self.task)
+    
+    def separate_audio(self):
+        output_path = os.getcwd()  # Use current directory as output path
+        
+        # Create output directory if it doesn't exist
+        os.makedirs(output_path, exist_ok=True)
+        
+        # Perform the separation
+        self.separator.separate_to_file(self.input_path, output_path)
+        
+    def run(self):
+        # Perform the separation
+        self.separate_audio()
+        
+        print("Separation completed")
+
+# Example usage
+#splitter = AdvanceSplitter('SS.mp3')
+#splitter.run()
