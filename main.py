ce972fbd874d76a5f422de1396414beadd315c62
NisaEngineers <71565114+NisaEngineers@users.noreply.github.com>
2025-07-30 07:11:34 +0600

    Update main.py

--- main.py
+++ main.py
@@ -163,4 +163,5 @@ async def download_file(full_path: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+
